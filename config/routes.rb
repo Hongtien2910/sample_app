c17d5e21052a212ad9bc0fc889326fd12e624ff0
Hongtien2910 <hongtien003@gmail.com>
2024-09-17 11:27:44 +0700

    chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "/static_pages/help"
     get "/static_pages/contact"
+    
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources  :users, only: %i(show)
   end
 end
