b1ebb10e403ecf21d43dadcf631f6c55e40f4293
Hongtien2910 <hongtien003@gmail.com>
2024-09-17 18:50:49 +0700

    chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
 
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
     root "static_pages#home"
     get "/static_pages/help"
     get "/static_pages/contact"
