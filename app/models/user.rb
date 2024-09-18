dfb80ec5e7d1617458c145a3a502446220ce1dcc
Hongtien2910 <hongtien003@gmail.com>
2024-09-18 20:10:21 +0700

    chapter9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   SIGN_UP_REQUIRE_ATTRIBUTES = %i(name email password
 password_confirmation).freeze
   before_save :downcase_email
+  attr_accessor :remember_token
 
   validates :name, presence: true,
             length: {maximum: Settings.sign_up.max_name}
@@ -25,8 +26,25 @@ password_confirmation).freeze
              end
       BCrypt::Password.create string, cost:
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
-  
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
   private
   def downcase_email
     email.downcase!
