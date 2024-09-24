887a423fc054d3dd9be98eb86d8301f09b5a76d0
Hongtien2910 <hongtien003@gmail.com>
2024-09-24 23:44:36 +0700

    chapter13

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module RailsTutorial
     config.load_defaults 7.0
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
+    config.active_storage.variant_processor = :mini_magick
 
     # Configuration for the application, engines, and railties goes here.
     #
